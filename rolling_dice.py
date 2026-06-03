6e9b7793f561502b034d155fe2e18b17a74990b6
benignUmbra <faizanusmani020@gmail.com>
2026-06-04 02:11:09 +0530

    Update rolling_dice.py

--- rolling_dice.py
+++ rolling_dice.py
@@ -1,17 +1,18 @@
-
-
 import random
 while True :
     choice=input("You want to roll dice(y/n): ").lower()
 
     if choice=="y":
         
-        num1=random.randint(1,6)
-        num2=random.randint(1,6)
+        #num1=random.randint(1,6)
+        #num2=random.randint(1,6)
         #print((num1,num2))
+                # OR
         #print(f"{(num1,num2)}")
-        
-        print(f"({random.randint(1,6)},{random.randint(1,6)})")
+                # OR
+        #print(f"({random.randint(1,6)},{random.randint(1,6)})")
+                # OR (Best)
+        print((random.randint(1,6),random.randint(1,6)))
 
     elif choice=="n":
         print("Bye♥!")
